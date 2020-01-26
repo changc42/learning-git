45108a38c4af3666eaa37d9028d34b19bd300d2a
Caleb Chang <changc42@gmail.com>
2020-01-25 20:38:43 -0500

    created a fake commit where everything is working perfectly

--- ex.js
+++
@@ -1,5 +0,0 @@
-this wont cauwegeg
-dsfdfdt cause any problems probably
-
-I was supposed to do this first.
-i am doing some work, but i realize i shouldnt be doing this work right now. I want to save this work for later but I dont want this to show up on the next commit.
